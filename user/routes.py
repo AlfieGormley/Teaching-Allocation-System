3c50929b851ad425ee60140ada044405ed9efdb6
AlfieGormley <alfiegyt@gmail.com>
2025-03-08 17:36:58 +0000

    Added Module Leader Calendar
    addition to ml.html

--- user/routes.py
+++ user/routes.py
@@ -9,48 +9,34 @@ def register():
     #Creates an instance of the User class from models.py and calls the register method
     return User().register()
 
-#At this url the login() method of the User() class is called
+
 @app.route('/user/login', methods=['POST'])
 def login():
-    #Creates an instance of the User class from models.py and calls the login method
     return User().login()
 
-#At this url the signout() method of the User() class is called
+
 @app.route('/user/signout')
 def signout():
-    #Creates an instance of the User class from models.py and calls the signout method
     return User().signout()
 
 @app.route('/user/update_skills', methods=['POST'])
 def update_skills():
-    #form_data = request.form
-    #print("Data from form:", form_data)
-    #return jsonify(success=True, message="Skills updated successfully")
-    
     return User().update_skills()
 
 
 @app.route('/user/remove_skills', methods=['POST'])
 def remove_skills():
-    #form_data = request.form
-    #print("Data from form:", form_data)
-    #return jsonify(success=True, message="Skills updated successfully")
-    
     return User().remove_skills()
 
 
 
 @app.route('/user/delete_user', methods=['POST'])
 def delete_user():
-    #form_data = request.form
-    #print("Data from form:", form_data)
-    #return jsonify(success=True, message="Skills updated successfully")
     return User().delete_user()
 
 
 @app.route('/user/change_password', methods=['POST'])
 def change_password():
-   
    return User().change_password()
 
 
@@ -63,16 +49,9 @@ def toggle_mobile():
    return User().toggle_mobile()
 
 
-#@app.route('/user/set_availability', methods=['POST'])
-#def set_availability():
-   
-#   return User().set_availability()
-
-
 
 @app.route('/user/set_availability', methods=['POST'])
 def set_availability():
-    
     return User().set_availability2()
     
     
